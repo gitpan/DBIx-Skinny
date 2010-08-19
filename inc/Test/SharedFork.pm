772bdd5506ab0307db86ec5c67e865f410d5fd7c
Atsushi Kobayashi <nekokak@cpan.org>
2010-08-19 09:48:57 +0000

    Import of NEKOKAK/DBIx-Skinny-0.0719 from CPAN.
    gitpan-cpan-distribution: DBIx-Skinny
    gitpan-cpan-version:      0.0719
    gitpan-cpan-path:         NEKOKAK/DBIx-Skinny-0.0719.tar.gz
    gitpan-cpan-author:       NEKOKAK
    gitpan-cpan-maturity:     released

--- inc/Test/SharedFork.pm
+++ inc/Test/SharedFork.pm
@@ -3,7 +3,7 @@ package Test::SharedFork;
 use strict;
 use warnings;
 use base 'Test::Builder::Module';
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 use Test::Builder 0.32; # 0.32 or later is needed
 use Test::SharedFork::Scalar;
 use Test::SharedFork::Array;
@@ -16,9 +16,10 @@ BEGIN {
     $STORE = Test::SharedFork::Store->new(
         cb => sub {
             my $store = shift;
-            tie __PACKAGE__->builder->{Curr_Test}, 'Test::SharedFork::Scalar', 0, $store;
+            tie __PACKAGE__->builder->{Curr_Test}, 'Test::SharedFork::Scalar', $store;
             tie @{ __PACKAGE__->builder->{Test_Results} }, 'Test::SharedFork::Array', $store;
-        }
+        },
+        builder => __PACKAGE__->builder,
     );
 
     no strict 'refs';
@@ -45,4 +46,4 @@ BEGIN {
 1;
 __END__
 
-#line 96
+#line 97
